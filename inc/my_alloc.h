e1f373a0b49669b6695e795b84d46a442c2e5876
Dragos <tantarudragos@yahoo.com>
2020-12-26 16:46:36 +0200

    finished setup for malloc

--- inc/my_alloc.h
+++ inc/my_alloc.h
@@ -6,12 +6,19 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 
+typedef enum{
+	VSMALL,
+	SMALL,
+	NORMAL
+}heap_type;
+
 typedef struct my_heap{
 	struct my_heap* prev;
 	struct my_heap* next;
+	heap_type		type;
 	size_t          all_size;
 	size_t          free_size;
-}d_heap;
+}d_heap; // tin metadate pt heap doar pt a eficientiza small heaps
 
 typedef struct my_block {
 	struct my_block* prev;
@@ -20,13 +27,22 @@ typedef struct my_block {
 	int				 free;
 }d_block;
 
+extern d_heap heap_top; // vreau aceiasi versiune a variabilei in tot proiectul
 
-#define META_SIZE sizeof(struct block_meta)
+#define META_BLOCK_SIZE sizeof(struct my_block)
+#define META_HEAP_SIZE  sizeof(struct my_heap)
+#define SMALL_BLOCK_SIZE  getpagesize() / 8 // in small heaps pun doar small blocks
+#define VSMALL_BLOCK_SIZE getpagesize() / 16 // la fel la very small
 
-struct block_meta* find_free_block(struct block_meta** last, size_t size);
-struct block_meta* request_space(struct block_meta* last, size_t size);
-
+// functiile importante
 void* my_alloc(size_t size);
 void* my_free(void* ptr);
+void* my_calloc(size_t count, size_t size);
+void* my_realloc(void* ptr, size_t newsize);
 
-
+// functii interne ale bibliotecii
+d_heap* search_for_free_heap(size_t size);
+d_block* search_for_free_block(size_t size, d_heap* heap);
+//void defragment_heap(d_heap* heap);
+void* create_heap(size_t size);
+void* append_block(size_t size, d_heap* heap);
