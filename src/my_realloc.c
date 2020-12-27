489a7bfec820fb1449b2727bb1ee7a3b964e9c72
Dragos <tantarudragos@yahoo.com>
2020-12-27 16:16:16 +0200

    fixed bugs on realloc + better realloc

--- src/my_realloc.c
+++ src/my_realloc.c
@@ -1,7 +1,9 @@
 /*
 	Functia de realocare seamana cu cea de alocare, in mod similar cauta heap-uri libere destul de
-	incapatoare. Daca nu gaseste, isi face un heap nou. Asta in cazul in care vrem un size mai mare,
-	daca size-ul cerut e mai mic, atunci pur si simplu adaugam
+	incapatoare. Daca nu gaseste, isi face un heap nou.Este adaugata functionalitate ca se uita, in
+	plus, si la blocul\spatiul liber de dupa blocul actual, sa vada daca poate sa-si ia si de acolo
+	spatiu. Asta in cazul in care vrem un size mai mare, daca size-ul cerut e mai mic, atunci pur si
+	simplu fac un split. Daca split-ul nu e posibil, nu	se face nicio modificare concreta.
 */
 
 #include "my_alloc.h"
@@ -28,23 +30,35 @@ my_realloc(void* ptr, size_t newsize)
 		// mai intai ma uit daca blocul liber dupa el e destul de mare
 		size_t remainder_size = newsize - block->size;
 		d_block* next_block = block->next;
-		if(next_block->size + sizeof(d_block) >= remainder_size)
+		d_heap* heap = get_heap_of_block(block);
+		if(next_block && next_block->size + sizeof(d_block) >= remainder_size)
 		{	// adica, daca putem lua blocul urmatorul (care e liber) si sa-l lipim de asta
 			block = merge_blocks(block, next_block);
 			block = split_block(newsize, block);
+			heap->free_size -= remainder_size - sizeof(d_block);
+			return (block + 1);
+		}
+		else if(next_block == NULL && heap->free_end_size >= remainder_size)
+		{	// daca avem in coada heap-ului spatiu nealocat destul
+			block->size = newsize; // il luam pur si simplu
+			heap->free_end_size -= remainder_size;
+			heap->free_size -= remainder_size;
 			return (block + 1);
 		}
 		else
 		{
-			void* new_add = my_alloc(newsize);
-			d_block* to_move = new_add - sizeof(d_block);
-			memcpy(new_add, block + 1, block->size);
-
+			size_t copy_for = block->size;
+			void* data = block + 1;
 			block->free = 1;
 			if(block->prev && block->prev->free)
 				block = merge_blocks(block->prev, block);
 			if(block->next && block->next->free)
 				block = merge_blocks(block, block->next);
+
+			void* new_add = my_alloc(newsize);
+			d_block* to_move = new_add - sizeof(d_block);
+			memcpy(new_add, data, copy_for);
+
 			return (to_move + 1);
 		}
 	}
